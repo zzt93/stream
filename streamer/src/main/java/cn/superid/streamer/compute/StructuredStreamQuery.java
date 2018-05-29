e687666aced04fab9ea0c78cc100615a814b5fb0
dufeng <dufeng@superid.cn>
2018-05-29 08:46:24 +0800

    set option failOnDataLoss=false

--- streamer/src/main/java/cn/superid/streamer/compute/StructuredStreamQuery.java
+++ streamer/src/main/java/cn/superid/streamer/compute/StructuredStreamQuery.java
@@ -6,8 +6,10 @@ import static org.apache.spark.sql.functions.count;
 import static org.apache.spark.sql.streaming.Trigger.ProcessingTime;
 
 import cn.superid.collector.entity.PageView;
+
 import java.io.Serializable;
 import java.util.stream.Collectors;
+
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
@@ -29,74 +31,74 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StructuredStreamQuery implements Serializable {
-  private static final Logger LOGGER = LoggerFactory.getLogger(StructuredStreamQuery.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(StructuredStreamQuery.class);
 
-  private final SparkSession spark;
-  private final String servers;
-  private final String streamerTopic;
-  private final String hdfsCheckpoint;
+    private final SparkSession spark;
+    private final String servers;
+    private final String streamerTopic;
+    private final String hdfsCheckpoint;
 
-  @Autowired
-  public StructuredStreamQuery(KafkaProperties kafkaProperties, SparkSession spark,
-      @Value("${streamer.kafka.minute}") String streamerTopic,
-      @Value("${streamer.hdfs.minute}") String hdfsCheckpoint) {
-    this.spark = spark;
-    servers = kafkaProperties.getBootstrapServers().stream().collect(
-        Collectors.joining(","));
-    this.streamerTopic = streamerTopic;
-    this.hdfsCheckpoint = hdfsCheckpoint;
-  }
-
-  public void run() throws StreamingQueryException {
-    Dataset<Row> df = spark
-        .readStream()
-        .format("kafka")
-        .option("kafka.bootstrap.servers", servers)
-        .option("subscribe", "collector.page")
-        .option("enable.auto.commit", true)
-        .option("auto.offset.reset", "latest")
-        .load();
-
-    Dataset<PageView> views = df.map(
-        (MapFunction<Row, PageView>) value -> {
-          LOGGER.info("value.get(1)="+value.get(1));
-          LOGGER.info("new String((byte[]) value.get(1))="+new String((byte[]) value.get(1)));
-          return PageView
-            .fromString(new String((byte[]) value.get(1)));
-        }, Encoders.bean(PageView.class));
-
-    Dataset<String> pageCounts = views
-//        .withWatermark("epoch", "1 minute")
-        .groupBy(functions.window(col("epoch"), "1 minute", "1 minute").as("epoch"))
-        .agg(count(col("*")).as("pv"), approx_count_distinct("viewId").alias("uv"),
-            approx_count_distinct("userId").alias("uvSigned"))
-        .withColumn("epoch", col("epoch.end"))
-        .toJSON().as("value");
-
-    for (StreamingQuery query : getStreamingQuery(pageCounts)) {
-      query.awaitTermination();
+    @Autowired
+    public StructuredStreamQuery(KafkaProperties kafkaProperties, SparkSession spark,
+                                 @Value("${streamer.kafka.minute}") String streamerTopic,
+                                 @Value("${streamer.hdfs.minute}") String hdfsCheckpoint) {
+        this.spark = spark;
+        servers = kafkaProperties.getBootstrapServers().stream().collect(
+                Collectors.joining(","));
+        this.streamerTopic = streamerTopic;
+        this.hdfsCheckpoint = hdfsCheckpoint;
     }
-  }
 
-  @SafeVarargs
-  private final StreamingQuery[] getStreamingQuery(Dataset<String>... datasets) {
-    StreamingQuery[] res = new StreamingQuery[datasets.length];
-    for (int i = 0; i < datasets.length; i++) {
-      res[i] = datasets[i]
-          .writeStream()
-          .outputMode("update")
-          .format("kafka")
-          .option("kafka.bootstrap.servers", servers)
-          .option("topic", streamerTopic)
-          .option("checkpointLocation", hdfsCheckpoint)
-          .trigger(ProcessingTime("20 seconds"))
+    public void run() throws StreamingQueryException {
+        Dataset<Row> df = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", servers)
+                .option("subscribe", "collector.page")
+                .option("enable.auto.commit", true)
+                .option("auto.offset.reset", "latest")
+                .option("failOnDataLoss", false)
+                .load();
+
+        Dataset<PageView> views = df.map(
+                (MapFunction<Row, PageView>) value -> {
+                    LOGGER.info("===new String((byte[]) value.get(1))=" + new String((byte[]) value.get(1)));
+                    return PageView
+                            .fromString(new String((byte[]) value.get(1)));
+                }, Encoders.bean(PageView.class));
+
+        Dataset<String> pageCounts = views
+//        .withWatermark("epoch", "1 minute")
+                .groupBy(functions.window(col("epoch"), "1 minute", "1 minute").as("epoch"))
+                .agg(count(col("*")).as("pv"), approx_count_distinct("viewId").alias("uv"),
+                        approx_count_distinct("userId").alias("uvSigned"))
+                .withColumn("epoch", col("epoch.end"))
+                .toJSON().as("value");
+
+        for (StreamingQuery query : getStreamingQuery(pageCounts)) {
+            query.awaitTermination();
+        }
+    }
+
+    @SafeVarargs
+    private final StreamingQuery[] getStreamingQuery(Dataset<String>... datasets) {
+        StreamingQuery[] res = new StreamingQuery[datasets.length];
+        for (int i = 0; i < datasets.length; i++) {
+            res[i] = datasets[i]
+                    .writeStream()
+                    .outputMode("update")
+                    .format("kafka")
+                    .option("kafka.bootstrap.servers", servers)
+                    .option("topic", streamerTopic)
+                    .option("checkpointLocation", hdfsCheckpoint)
+                    .trigger(ProcessingTime("20 seconds"))
 //          .format("console")
 //          .option("truncate", false)
 //          .option("numRows", 50)
-          .start();
+                    .start();
+        }
+        return res;
     }
-    return res;
-  }
 
 
 }
