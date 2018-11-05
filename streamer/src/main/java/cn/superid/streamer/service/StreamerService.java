0c5ea279c6ac25c3efe0d4f7c2c726e26885eb92
zzt <zengzt93@gmail.com>
2018-11-05 17:41:38 +0800

    refactor page info with more info.

--- streamer/src/main/java/cn/superid/streamer/service/StreamerService.java
+++ streamer/src/main/java/cn/superid/streamer/service/StreamerService.java
@@ -1,25 +1,32 @@
 package cn.superid.streamer.service;
 
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.count;
+import static org.apache.spark.sql.functions.countDistinct;
+import static org.apache.spark.sql.functions.lit;
+
+import cn.superid.collector.entity.view.PageView;
 import cn.superid.collector.entity.view.RichPageStatistic;
+import cn.superid.streamer.compute.MongoConfig;
+import cn.superid.streamer.compute.Unit;
 import cn.superid.streamer.form.RichForm;
+import com.mongodb.spark.MongoSpark;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.sql.Timestamp;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
 
 /**
  * @author dufeng
@@ -27,143 +34,70 @@ import java.util.*;
  */
 @Service
 public class StreamerService {
-    private final Logger logger = LoggerFactory.getLogger(StreamerService.class);
-    /**
-     * 限制前端查询时间范围内的时间点个数
-     */
-    private static final int MINUTES_COUNT_LIMIT = 500;
-    private static final int HOURS_COUNT_LIMIT = 500;
-    private static final int DAYS_COUNT_LIMIT = 500;
 
-    @Autowired
-    private MongoTemplate mongo;
+  /**
+   * 限制前端查询时间范围内的时间点个数
+   */
+  private static final int COUNT_LIMIT = 100;
+  private final Logger logger = LoggerFactory.getLogger(StreamerService.class);
+  private final Dataset<PageView> pageView;
 
-    @Value("${collector.mongo.day.rich}")
-    private String dayRich;
-    @Value("${collector.mongo.hour.rich}")
-    private String hourRich;
-    @Value("${collector.mongo.minute.rich}")
-    private String minuteRich;
+  @Autowired
+  public StreamerService(SparkSession spark, @Value("${collector.mongo.page}") String pages) {
+    //把mongodb中的数据加载到spark中
+    pageView = MongoSpark.load(spark, MongoConfig.readConfig(spark, pages), PageView.class);
+    //创建spark的临时视图，用于查询
+    pageView.createOrReplaceTempView(pages);
+  }
 
-    /**
-     * 以分钟为单位获取pv uv信息
-     *
-     * @param richForm
-     * @return
-     */
-    public List<RichPageStatistic> rangeRichPageviewsInMinutes(@RequestBody RichForm richForm) {
-        LocalDateTime fromLocalDateTime = richForm.getFrom().toLocalDateTime().truncatedTo(ChronoUnit.MINUTES);
-        LocalDateTime toLocalDateTime = richForm.getTo().toLocalDateTime().truncatedTo(ChronoUnit.MINUTES);
-        if (toLocalDateTime.isAfter(LocalDateTime.now())) {
-            toLocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        }
+  public List<RichPageStatistic> rangeRichPageviewsInUnit(@RequestBody RichForm richForm,
+      Unit unit) {
+    LocalDateTime from = unit.truncate(richForm.getFrom().toLocalDateTime());
 
-        long minutesCount = Duration.between(fromLocalDateTime, toLocalDateTime).toMinutes();
-        if ( minutesCount > MINUTES_COUNT_LIMIT) {
-            System.out.println("查询时间范围内包含的分钟时间点过多:"+minutesCount);
-            return Collections.emptyList();
-        }
-
-        return getRichPageStatistics(fromLocalDateTime, toLocalDateTime, richForm, minuteRich);
+    long timeDiff = unit.getDifferenceUnit(richForm.getFrom(), richForm.getTo());
+    if (timeDiff > COUNT_LIMIT) {
+      logger.info("查询时间范围内包含的{}过多:{}", unit, timeDiff);
+      return Collections.emptyList();
     }
 
+    return getRichPageStatistics(from, (int) timeDiff, unit, richForm);
+  }
 
-    /**
-     * 以小时为单位获取pv uv信息
-     *
-     * @param richForm
-     * @return
-     */
-    public List<RichPageStatistic> rangeRichPageviewsInHours(@RequestBody RichForm richForm) {
-        LocalDateTime fromLocalDateTime = richForm.getFrom().toLocalDateTime().truncatedTo(ChronoUnit.HOURS);
-        LocalDateTime toLocalDateTime = richForm.getTo().toLocalDateTime().truncatedTo(ChronoUnit.HOURS);
-        if (toLocalDateTime.isAfter(LocalDateTime.now())) {
-            toLocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
-        }
 
-        long hoursCount = Duration.between(fromLocalDateTime, toLocalDateTime).toHours();
-        if (hoursCount > HOURS_COUNT_LIMIT) {
-            System.out.println("查询时间范围内包含的小时时间点过多:" + hoursCount);
-            return Collections.emptyList();
-        }
+  private List<RichPageStatistic> getRichPageStatistics(LocalDateTime from, int timeDiff,
+      Unit unit, RichForm query) {
 
-        return getRichPageStatistics(fromLocalDateTime, toLocalDateTime, richForm, hourRich);
+    List<Column> group = new ArrayList<>();
+    pageView.where(col("publicIp").equalTo(true));
+
+    if (query.getAffairId() != null) {
+      pageView.where(col("affairId").equalTo(query.getAffairId()));
+      group.add(col("affairId"));
+    }
+    if (query.getTargetId() != null) {
+      pageView.where(col("targetId").equalTo(query.getTargetId()));
+      group.add(col("targetId"));
+    }
+    if (!StringUtils.isEmpty(query.getDevType())) {
+      pageView.where(col("deviceType").equalTo(query.getDevType()));
+      group.add(col("deviceType"));
     }
 
-    /**
-     * 以天为单位获取pv uv信息
-     *
-     * @param richForm
-     * @return
-     */
-    public List<RichPageStatistic> rangeRichPageviewsInDays(@RequestBody RichForm richForm) {
-        LocalDateTime fromLocalDateTime = richForm.getFrom().toLocalDateTime().truncatedTo(ChronoUnit.DAYS);
-        LocalDateTime toLocalDateTime = richForm.getTo().toLocalDateTime().truncatedTo(ChronoUnit.DAYS);
-        if (toLocalDateTime.isAfter(LocalDateTime.now())) {
-            toLocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
-        }
+    Timestamp low = Timestamp.valueOf(from);
+    for (int offset = 0; offset < timeDiff; offset++) {
+      Timestamp upper = Timestamp.valueOf(unit.update(low, offset + 1));
+      pageView.where(col("epoch").between(low, upper));
 
-        long daysCount = Duration.between(fromLocalDateTime, toLocalDateTime).toDays();
-        if (daysCount > DAYS_COUNT_LIMIT) {
-            System.out.println("查询时间范围内包含的天时间点过多:"+daysCount);
-            return Collections.emptyList();
-        }
-
-        return getRichPageStatistics(fromLocalDateTime, toLocalDateTime, richForm, dayRich);
+      Dataset<RichPageStatistic> stat = pageView
+          .groupBy(group.toArray(new Column[0]))
+          .agg(count("*").as("pv"), countDistinct(col("viewId")).as("uv"),
+              countDistinct(col("userId")).as("uvSigned"))
+          .withColumn("epoch", lit(upper))
+          .as(Encoders.bean(RichPageStatistic.class));
+      Object take = stat.take(0);
+      logger.info("{}", take);
     }
 
-
-    private List<RichPageStatistic> getRichPageStatistics(LocalDateTime fromLocalDateTime, LocalDateTime toLocalDateTime,
-                                                          RichForm richForm, String collectionName) {
-
-        //把前端传过来的条件放到列表中，然后转换成一个变长数组传递给andOperator方法
-        List<Criteria> criteriaList = new ArrayList<>();
-        criteriaList.add(Criteria.where("epoch").gt(Timestamp.valueOf(fromLocalDateTime)));
-        criteriaList.add(Criteria.where("epoch").lte(Timestamp.valueOf(toLocalDateTime)));
-        if(richForm.getAffairId()>0){
-            criteriaList.add(Criteria.where("affairId").is(richForm.getAffairId()));
-        }
-        if(richForm.getTargetId()>0){
-            criteriaList.add(Criteria.where("targetId").is(richForm.getTargetId()));
-        }
-        if (!StringUtils.isEmpty(richForm.getDevType())) {
-            criteriaList.add(Criteria.where("deviceType").is(richForm.getDevType()));
-        }
-
-        Criteria[] criterias = new Criteria[criteriaList.size()];
-        for(int i = 0 ;i < criterias.length;i++){
-            criterias[i] = criteriaList.get(i);
-        }
-        Criteria timeCriteria = new Criteria().andOperator(criterias);
-        //查询好像不可以有多个andOperator，所以把多个Criteria放进一个数组中传给一个andOperator
-
-        Criteria ipCriteria = Criteria.where("publicIp").is(true);
-        Query query = Query.query(new Criteria().andOperator(timeCriteria, ipCriteria)).with(Sort.by(Sort.Direction.ASC, "epoch"));
-
-        LinkedList<RichPageStatistic> pageStatistics = new LinkedList<>(mongo.find(query, RichPageStatistic.class, collectionName));
-
-//        ListIterator<RichPageStatistic> it = pageStatistics.listIterator();
-//        for (int i = pageStatistics.size() - 1; i >= 0; i--) {
-//            LocalDateTime time = null;
-//            if("minute".equals(richForm.getTimeUnit())){
-//                time = toLocalDateTime.minusMinutes(i);
-//            } else if ("hour".equals(richForm.getTimeUnit())) {
-//                time = toLocalDateTime.minusHours(i);
-//            } else if ("day".equals(richForm.getTimeUnit())) {
-//                time = toLocalDateTime.minusDays(i);
-//            }
-//
-//            boolean hasMore = it.hasNext();
-//            if (hasMore && time.atZone(ZoneOffset.systemDefault()).toInstant().toEpochMilli() == it.next().getId()) {
-//            } else {
-//                if (it.hasPrevious() && hasMore) {
-//                    it.previous();
-//                }
-//                RichPageStatistic pageStatistic = new RichPageStatistic(Timestamp.valueOf(time));
-//                it.add(pageStatistic);
-//            }
-//        }
-
-        return pageStatistics;
-    }
+    return null;
+  }
 }
