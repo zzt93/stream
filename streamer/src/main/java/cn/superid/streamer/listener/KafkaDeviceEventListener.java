b6d394599a83463f79bfc05bee512ce83d856508
wxf <wuxiufeng@superid.cn>
2019-06-17 10:11:55 +0800

    [fix]kafka listener

--- streamer/src/main/java/cn/superid/streamer/listener/KafkaDeviceEventListener.java
+++ streamer/src/main/java/cn/superid/streamer/listener/KafkaDeviceEventListener.java
@@ -4,11 +4,21 @@ import cn.superid.streamer.constant.OperationType;
 import cn.superid.streamer.dto.KafkaDeviceDTO;
 import cn.superid.streamer.service.StreamerService;
 import com.alibaba.fastjson.JSON;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -18,6 +28,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaDeviceEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaDeviceEventListener.class);
 
     private static final String TOPIC_ONLINE = "device_online";
     private static final String TOPIC_OFFLINE = "device_offline";
@@ -25,9 +36,25 @@ public class KafkaDeviceEventListener {
     @Autowired
     private StreamerService streamerService;
 
-    @KafkaListener(topics = "${spring.kafka.topic-prefix}" + TOPIC_OFFLINE)
+    private Map<String, Object> consumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return props;
+    }
+
+    @Bean("ackContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory ackContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory(consumerProps()));
+        factory.getContainerProperties().setAckMode(AbstractMessageListenerContainer.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
+    @KafkaListener(id = "streamer", topics = "${spring.kafka.topic-prefix}" + TOPIC_OFFLINE,
+            containerFactory = "ackContainerFactory")
     public void deviceOfflineListener(ConsumerRecord<Long, String> record,
                                       Acknowledgment ack) throws Exception {
+        logger.info("deviceOfflineListener, record={}", record.toString());
         KafkaDeviceDTO deviceDTO = JSON.parseObject(record.value(), KafkaDeviceDTO.class);
         if (deviceDTO != null) {
             streamerService.userOperation(deviceDTO, OperationType.offline);
@@ -35,9 +62,11 @@ public class KafkaDeviceEventListener {
         ack.acknowledge();
     }
 
-    @KafkaListener(topics = "${spring.kafka.topic-prefix}" + TOPIC_ONLINE)
+    @KafkaListener(id = "streamer", topics = "${spring.kafka.topic-prefix}" + TOPIC_ONLINE,
+            containerFactory = "ackContainerFactory")
     public void deviceOnlineListener(ConsumerRecord<Long, String> record,
                                      Acknowledgment ack) throws Exception {
+        logger.info("deviceOnlineListener, record={}", record.toString());
         KafkaDeviceDTO deviceDTO = JSON.parseObject(record.value(), KafkaDeviceDTO.class);
 
         if (deviceDTO != null) {
